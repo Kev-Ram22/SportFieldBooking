37192e10ef861295dabf74c661af36fc323cdff3
Kev-Ram22 <kevinre1122@gmail.com>
2024-11-21 00:48:55 -0500

    Creacion del Modelo Campo

---
+++ SportFieldBooking/Models/Campo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportFieldBooking.Models
+{
+	public class Campo
+	{
+		[Key]
+		public int IdCampo { get; set; }
+		public string Nombre { get; set; }
+		public string Tipo { get; set; }
+		public string Ubicación { get; set; }
+
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal TarifaHora { get; set; }
+
+		public ICollection<Reserva> Reservas { get; set; }
+		public ICollection<Evento> Eventos { get; set; }
+	}
+}
--- SportFieldBooking/obj/Debug/net8.0/SportFieldBooking.AssemblyInfo.cs
+++ SportFieldBooking/obj/Debug/net8.0/SportFieldBooking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportFieldBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d7a31fa3db6ebe137ca14f9c675f31b2390b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportFieldBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportFieldBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
