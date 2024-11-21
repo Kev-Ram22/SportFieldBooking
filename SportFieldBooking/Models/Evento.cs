9b39a63bd0f844be9497604815ebbf374779a218
Kev-Ram22 <kevinre1122@gmail.com>
2024-11-21 00:54:42 -0500

    Creacion del Modelo Evento y Pago

---
+++ SportFieldBooking/Models/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportFieldBooking.Models
+{
+	public class Evento
+	{
+		[Key]
+		public int IdEvento { get; set; }
+		public int IdCampo { get; set; } // FK
+		public string NombreEvento { get; set; }
+		public DateTime FechaEvento { get; set; }
+		public TimeSpan HoraInicio { get; set; }
+		public TimeSpan HoraFin { get; set; }
+		public string Descripción { get; set; }
+
+		// Relación
+		public Campo Campo { get; set; }
+	}
+}
---
+++ SportFieldBooking/Models/Pago.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportFieldBooking.Models
+{
+	public class Pago
+	{
+		[Key]
+		public int IdPago { get; set; }
+		public int IdReserva { get; set; } // FK
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal Monto { get; set; }
+		public DateTime FechaPago { get; set; }
+		public string MétodoPago { get; set; } // Tarjeta, Efectivo
+
+		// Relación
+		public Reserva Reserva { get; set; }
+	}
+}
